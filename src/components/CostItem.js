e2406e051f47b769a83721e1b6b9b9435ab72cc1
parom <parom.evgen@mail.ru>
2023-06-04 10:51:03 +0300

    separare Date component with CSS

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,22 +1,13 @@
+import CostDate from './CostDate';
 import './CostItem.css'
 
 function CostItem(props) {
 
-    const month = props.date.toLocaleString('ru-Ru', {
-        month: 'long'
-    });
-    const year = props.date.getFullYear();
-    const day = props.date.toLocaleString('ru-Ru', {
-        day: "2-digit"
-    });
+ 
 
     return (
         <div className='cost-item'>
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <CostDate date={props.date}/>
             <div className='cost-item__description'>
                 <h2>{props.description}</h2>
                 <div className='cost-item__price'>${props.amount}</div>
