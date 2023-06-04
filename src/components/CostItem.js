de1626b1a3b798109cbd244edd193084de66092e
parom <parom.evgen@mail.ru>
2023-06-04 11:43:55 +0300

    new component refactoring

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -3,15 +3,13 @@ import './CostItem.css'
 
 function CostItem(props) {
 
- 
-
     return (
         <div className='cost-item'>
             <CostDate date={props.date}/>
             <div className='cost-item__description'>
-                <h2>{props.description}</h2>
-                <div className='cost-item__price'>${props.amount}</div>
-            </div>            
+                <h2>{props.description}</h2>                
+            </div> 
+            <div className='cost-item__price'>${props.amount}</div>           
         </div>        
     );
 }
---
+++ src/components/Costs.js
@@ -0,0 +1,26 @@
+import CostItem from './CostItem';
+import './Costs.css'
+
+function Costs(props) {
+
+    const costs = props.costs;
+
+    return(
+        <div className='costs'>
+            <CostItem 
+                date={costs[0].date}
+                description={costs[0].description}
+                amount={costs[0].amount}/>
+            <CostItem
+                date={costs[1].date}
+                description={costs[1].description}
+                amount={costs[1].amount}/>
+            <CostItem
+                date={costs[2].date}
+                description={costs[2].description}
+                amount={costs[2].amount}/>           
+        </div>
+    );
+}
+
+export default Costs;
