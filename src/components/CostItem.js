eb75566236d62c2eae3810a96a4fed869e2f3e5f
parom <parom.evgen@mail.ru>
2023-06-04 09:16:56 +0300

    add CSS to react Component

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,6 +1,14 @@
+import './CostItem.css'
+
 function CostItem() {
     return (
-        <h2>Элемент расхода</h2>
+        <div className='cost-item'>
+            <div>март 2021 12</div>
+            <div className='cost-item__description'>
+                <h2>Холодильник</h2>
+                <div className='cost-item__price'>$999.99</div>
+            </div>            
+        </div>        
     );
 }
 
