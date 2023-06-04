caed02beaa910e93fa883edbcbf51e6de5852eee
parom <parom.evgen@mail.ru>
2023-06-04 10:03:51 +0300

    Props in Components

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,17 +1,13 @@
 import './CostItem.css'
 
-function CostItem() {
-
-    const costDate = new Date(2021, 2, 12);
-    const costName = 'Холодильник';
-    const costAmount = 999.99;
+function CostItem(props) {
 
     return (
         <div className='cost-item'>
-            <div>{costDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='cost-item__description'>
-                <h2>{costName}</h2>
-                <div className='cost-item__price'>${costAmount}</div>
+                <h2>{props.description}</h2>
+                <div className='cost-item__price'>${props.amount}</div>
             </div>            
         </div>        
     );
