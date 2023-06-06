4fd5fa5a3d7c860a37185029094bb92016b77210
parom <E.Parominskiy@isida.by>
2023-06-06 16:09:18 +0300

    several states in one component

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,24 +1,40 @@
 import './CostForm.css'
+import Reactimport, { useState } from 'react';
 
 const CostForm = () => {
 
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
     const nameChangeHandler = (event) => {
         console.log(event.target.value);
+        setName(event.target.value)
+    }
+
+    const ammountChangeHandler = (event) => {
+        console.log(event.target.value);
+        setAmount(event.target.value)
+    }
+
+    const dateChangeHandler = (event) => {
+        console.log(event.target.value);
+        setDate(event.target.value)
     }
 
     return <form>
         <div className="new-cost__controls">
             <div className="new-cost__controls">
                 <label>Название</label>
-                <input type="text" onChange={nameChangeHandler}/>
+                <input type="text" onChange={nameChangeHandler} />
             </div>
             <div className="new-cost__controls">
                 <label>Сумма</label>
-                <input type="number" min='0.01' step='0.01' />
+                <input type="number" min='0.01' step='0.01' onChange={ammountChangeHandler} />
             </div>
             <div className="new-cost__controls">
                 <label>Дата</label>
-                <input type="date" min='2019-01-01' max='2023-12-31' />
+                <input type="date" min='2019-01-01' max='2023-12-31' onChange={dateChangeHandler} />
             </div>
             <div className="new-cost__actions">
                 <button type='submit'>Добавить расход</button>
