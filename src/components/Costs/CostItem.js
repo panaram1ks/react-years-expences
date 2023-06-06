d4c8d1f3da99ec57f166c0da065d9226a706d22f
parom <E.Parominskiy@isida.by>
2023-06-06 08:42:48 +0300

    state useState, update Component and data

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,16 +1,23 @@
 import CostDate from './CostDate';
 import './CostItem.css';
 import Card from '../UI/Card';
+import React, { useState } from 'react';
 
 const CostItem = (props) => {
 
-    const changeDescriptionHandler = () => {console.log("Click!");}
+    const[description, setDescription] = useState(props.description);    
+
+    const changeDescriptionHandler = () => {
+      /*   description = 'New text' */
+        setDescription('New text')
+        console.log(description);
+    }
 
     return (
         <Card className='cost-item'>
             <CostDate date={props.date} />
             <div className='cost-item__description'>
-                <h2>{props.description}</h2>
+                <h2>{description}</h2>
             </div>
             <div className='cost-item__price'>${props.amount}</div>
             <button onClick={changeDescriptionHandler}>Изменить Описание</button>
