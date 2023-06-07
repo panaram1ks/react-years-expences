753811d45918058e809acbff38f41ef23b44f7ca
parom <parom.evgen@mail.ru>
2023-06-07 19:43:14 +0300

    show content by condition

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -11,23 +11,38 @@ const Costs = (props) => {
     /* const costs = props.costs; */
 
     const filteredCosts = props.costs.filter(cost => {
-       return cost.date.getFullYear().toString() === inputYear
+        return cost.date.getFullYear().toString() === inputYear
     })
 
     const selectYearHandler = (year) => {
         setInputYear(year)
     }
 
+    let costsContent = <p>В этом году расходов нет!</p>
+
+    if (filteredCosts.length > 0) {
+        costsContent = filteredCosts.map(
+            (cost) => <CostItem
+                key={cost.id}
+                date={cost.date}
+                description={cost.description}
+                amount={cost.amount} />
+        )
+    }
+
     return (
         <Card className='costs'>
             <CostsFilter onChangeYear={selectYearHandler} startYear={inputYear} />
-            {filteredCosts.map(
+            {/*  {filteredCosts.length === 0 ? <p>В этом году расходов нет!</p> : filteredCosts.map(
                 (cost) => <CostItem
                     key={cost.id}
                     date={cost.date}
                     description={cost.description}
                     amount={cost.amount} />
-            )}
+            )} */}
+
+            {costsContent}
+
         </Card>
     );
 }
