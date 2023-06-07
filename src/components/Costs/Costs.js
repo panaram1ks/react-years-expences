2cb73e3bd0421def5fcfc195b92da88dff4bf6fd
parom <E.Parominskiy@isida.by>
2023-06-07 15:37:11 +0300

    should always add key atribute when use List of components

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -19,6 +19,7 @@ const Costs = (props) => {
             <CostsFilter onChangeYear={selectYearHandler} startYear={inputYear} />
             {props.costs.map(
                 (cost) => <CostItem
+                    key={cost.id}
                     date={cost.date}
                     description={cost.description}
                     amount={cost.amount} />
