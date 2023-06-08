1665254a36d4832af04888292df4770ec0b108de
parom <E.Parominskiy@isida.by>
2023-06-08 09:16:03 +0300

    dinamic change style!

--- src/components/Diagram/DiagramBar.js
+++ src/components/Diagram/DiagramBar.js
@@ -4,13 +4,15 @@ const DiagramBar = (props) => {
 
     let barFillHeight = '0%'
 
-    if(props.maxValue > 0){
-        barFillHeight = Math.round(props.value / props.maxValue * 100) + '%'        
+    if (props.maxValue > 0) {
+        barFillHeight = Math.round(props.value / props.maxValue * 100) + '%'
     }
 
     return <div className='diagram-bar'>
         <div className='diagram-bar__inner'>
-            <div className='diagram-bar__fill'></div>
+            <div className='diagram-bar__fill' style={
+                { height: barFillHeight }
+            }></div>
         </div>
         <div className='diagram-bar__lable'>{props.lable}</div>
     </div>
